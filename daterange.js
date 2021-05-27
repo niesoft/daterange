fa51192ddc74b241caec68686e0ced7f80347bd9
Niesoft <vdonsk@bk.ru>
2021-05-27 03:34:19 +0300

    добавил основной скрипт

---
+++ daterange.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    daterange.Init();
+});
+
+var daterange = {
+    Init: function()
+    {
+        document.querySelectorAll(".daterange").forEach(item => {
+            console.log(item);
+        });
+    },
+    Create: function(elem)
+    {
+        
+    }
+};
